6b8860864869fcd19d970616fb71698e4fcafb91
Amarkumar Belkhede <belkhedeamar@gmail.com>
2023-07-17 14:17:09 +0530

    incomplete protobuf

--- ProtoDatastore/app/src/main/java/com/example/protodatastore/MainActivity.kt
+++ ProtoDatastore/app/src/main/java/com/example/protodatastore/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.protodatastore
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,9 +11,46 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import androidx.datastore.migrations.SharedPreferencesMigration
+import androidx.datastore.migrations.SharedPreferencesView
+import com.example.protodatastore.UserPreferences.SortOrder
+import com.example.protodatastore.data.UserPreferencesSerializer
 import com.example.protodatastore.ui.theme.ProtoDatastoreTheme
 
+private const val USER_PREFERENCES_NAME = "user_preferences"
+private const val DATA_STORE_FILE_NAME = "user_prefs.pb"
+private const val SORT_ORDER_KEY = "sort_order"
+
+// Build the DataStore
+private val Context.userPreferencesStore: DataStore<UserPreferences> by dataStore(
+    fileName = DATA_STORE_FILE_NAME,
+    serializer = UserPreferencesSerializer,
+    produceMigrations = { context ->
+        listOf(
+            SharedPreferencesMigration(
+                context,
+                USER_PREFERENCES_NAME
+            ) { sharedPrefs: SharedPreferencesView, currentData: UserPreferences ->
+                // Define the mapping from SharedPreferences to UserPreferences
+                if (currentData.sortOrder == SortOrder.UNSPECIFIED) {
+                    currentData.toBuilder().setSortOrder(
+                        SortOrder.valueOf(
+                            sharedPrefs.getString(SORT_ORDER_KEY, SortOrder.NONE.name)!!
+                        )
+                    ).build()
+                } else {
+                    currentData
+                }
+            }
+        )
+    }
+)
+
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
---
+++ ProtoDatastore/app/src/main/java/com/example/protodatastore/data/UserPreferencesSerializer.kt
@@ -0,0 +1,27 @@
+package com.example.protodatastore.data
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.example.protodatastore.UserPreferences
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+/**
+ * Serializer for the [UserPreferences] object defined in user_prefs.proto.
+ */
+object UserPreferencesSerializer : Serializer<UserPreferences> {
+    override val defaultValue: UserPreferences = UserPreferences.getDefaultInstance()
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    override suspend fun readFrom(input: InputStream): UserPreferences {
+        try {
+            return UserPreferences.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    override suspend fun writeTo(t: UserPreferences, output: OutputStream) = t.writeTo(output)
+}
