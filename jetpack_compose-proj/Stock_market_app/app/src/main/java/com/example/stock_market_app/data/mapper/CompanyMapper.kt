06a529dc6a7239ff5cd4922b0d1107796357b7a9
Amarkumar Belkhede <belkhedeamar@gmail.com>
2023-07-31 20:09:05 +0530

    half backed cake

---
+++ jetpack_compose-proj/Stock_market_app/app/src/main/java/com/example/stock_market_app/data/mapper/CompanyMapper.kt
@@ -0,0 +1,31 @@
+package com.example.stock_market_app.data.mapper
+
+import com.example.stock_market_app.data.local.CompanyListingEntity
+import com.example.stock_market_app.domain.model.CompanyListing
+
+
+fun CompanyListingEntity.toCompanyListing(): CompanyListing {
+    return CompanyListing(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
+
+fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
+    return CompanyListingEntity(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
+
+fun CompanyInfoDto.toCompanyInfo(): CompanyInfo {
+    return CompanyInfo(
+        symbol = symbol ?: "",
+        description = description ?: "",
+        name = name ?: "",
+        country = country ?: "",
+        industry = industry ?: ""
+    )
+}
---
+++ jetpack_compose-proj/Stock_market_app/app/src/main/java/com/example/stock_market_app/data/remote/StockApi.kt
@@ -0,0 +1,19 @@
+package com.example.stock_market_app.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apiKey: String
+    ): ResponseBody
+
+    companion object{
+        const val API_KEY = "KN26RSYNRV52KLCX"
+        const val BASE_URL = "https://alphavantage.co"
+
+    }
+}
