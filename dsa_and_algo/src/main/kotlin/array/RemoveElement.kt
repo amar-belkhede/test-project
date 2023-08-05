06d8dcbe4d74e9585d74f6a38e46e8b7b9ee35a3
Amarkumar Belkhede <belkhedeamar@gmail.com>
2023-08-05 23:11:26 +0530

    complete remove element

--- dsa_and_algo/src/main/kotlin/array/RemoveElement.kt
+++ dsa_and_algo/src/main/kotlin/array/RemoveElement.kt
@@ -2,6 +2,26 @@ package array
 
 class RemoveElement {
     companion object {
-        
+        fun at(nums: Array<Int>, target: Int): Int {
+            var i = 0;
+            var n = nums.size
+            while (i<n) {
+                if (nums[i]==target) {
+                    nums[i] = nums[n-1]
+                    nums[n-1] = -1 // this line is of no need just to show
+                    n--
+                } else {
+                    i++
+                }
+            }
+
+            println(nums.contentToString())
+
+            return n;
+        }
     }
+}
+
+fun main() {
+    println(RemoveElement.at(arrayOf(0,1,3,2,2,4,5,2), 2))
 }
