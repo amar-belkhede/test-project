c0d2717bdc3decc62283cb89bde15d649944e221
Amarkumar Belkhede <belkhedeamar@gmail.com>
2023-08-05 23:43:49 +0530

    remove dupicate element from sorted array

--- dsa_and_algo/src/main/kotlin/array/RemoveElement.kt
+++ dsa_and_algo/src/main/kotlin/array/RemoveElement.kt
@@ -19,9 +19,26 @@ class RemoveElement {
 
             return n;
         }
+
+        fun duplicateFromSortedArray(nums: Array<Int>): Int {
+            if (nums.isEmpty()) return 0;
+            var i = 0
+            var j=1
+            for (j in j until nums.size) {
+                if (nums[i] != nums[j]) {
+                    i++
+                    nums[i] = nums[j]
+                }
+            }
+
+            println(nums.contentToString())
+
+            return i+1;
+        }
     }
 }
 
 fun main() {
-    println(RemoveElement.at(arrayOf(0,1,3,2,2,4,5,2), 2))
+//    println(RemoveElement.at(arrayOf(0,1,3,2,2,4,5,2), 2))
+    println(RemoveElement.duplicateFromSortedArray(arrayOf(1,2,2,3,3,3,4,5,6,77)))
 }
